4c9e362169616e68a8e2d50cfe42dac782ee3714
thnikk <thnikk@users.noreply.github.com>
2022-04-22 13:31:15 -0700

    Initial commit

---
+++ code.py
@@ -0,0 +1,55 @@
+# Keypad libraries
+import keypad
+import board
+import usb_hid
+from adafruit_hid.keyboard import Keyboard, find_device
+from adafruit_hid.keycode import Keycode
+
+# LED libraries
+import neopixel
+from rainbowio import colorwheel
+pixels = neopixel.NeoPixel(board.GP0, 2, brightness=1, auto_write=False)
+logo = neopixel.NeoPixel(board.GP12, 2, brightness=1, auto_write=False)
+
+# Time (for LED timing)
+import time
+
+# Logo LED requires power from internal GP11 pin
+logo_power = digitalio.DigitalInOut(board.GP11)
+logo_power.direction = digitalio.Direction.OUTPUT
+logo_power.value = 1
+
+# Set pins for keys
+pins = ( board.D1, board.D2, board.D3, board.D4, board.D5, board.D6, board.D7, board.D8 )
+keys = keypad.Keys(pins, value_when_pressed=False, pull=True, interval=0.020)
+
+# Map position  and keycodes
+keymap = { 0:7, 1:6, 2:5, 3:2, 4:3, 5:4, 6:0, 7:1 }
+keycodes = [ Keycode.Z, Keycode.X, Keycode.C, Keycode.A, Keycode.S, Keycode.D, Keycode.Q, Keycode.W ]
+
+
+# Millis timer (float) for LEDs
+ledMillis = 0;
+hue = 0;
+
+while True:
+
+    # Update once every 20 milliseconds
+    if (time.monotonic() - ledMillis) >= 0.020:
+        ledMillis = time.monotonic() # Reset timer
+        hue+=1 # Increment hue by 1
+        pixels.fill(colorwheel(hue % 255) # Get full RGB from hue value
+        pixels.show() # Update LEDs
+        logo[0] = (255,255,255) # Set logo color to white
+        logo.show() # Update logo LED
+
+    # Get new keypad events
+    ev = keys.events.get()
+    if ev is not None:
+        # Get keycode for key
+        key = keycodes[keymap[ev.key_number]]
+        # Press or release on events
+        if ev.pressed:
+            kbd.press(key)
+        else:
+            kbd.release(key)
---
+++ readme.md
@@ -0,0 +1,20 @@
+# Afpad Circuitpython
+This is a basic firmware written for the Afpad using Circuitpython. I don't have the hardware and can't test the firmware, but it should work.
+
+## Installation
+Download the bootloader from [Adafruit](https://circuitpython.org/board/seeeduino_xiao_rp2040/) and copy it to the xiao by holding down the bootsel button (the one with the B next to it) while plugging it in. Copy the UF2 bootloader to the "RP_RP2" removable drive and it will flash the new bootloader and remount itself as a CircuitPython device.
+
+Copy everything from this repository to the newly mounted CIRCUITPY device and it should update in real time.
+
+## Features
+It's got some rainbow leds keys.
+
+## Resources
+Adafruit documents all of their libraries in the [CircuitPython documentation](https://docs.circuitpython.org/en/latest/docs/index.html). Look up specific libraries for more detail.
+
+[Keypad library](https://docs.circuitpython.org/en/latest/shared-bindings/keypad/index.html)
+[Keyboard library](https://docs.circuitpython.org/projects/hid/en/latest/api.html)
+[Keycodes](https://docs.circuitpython.org/projects/hid/en/latest/_modules/adafruit_hid/keycode.html)
+[Neopixel library](https://docs.circuitpython.org/projects/neopixel/en/latest/api.html?highlight=neopixel#neopixel-neopixel-strip-driver)
+
+For more basic resources, check out a site like [w3schools](https://www.w3schools.com/python/default.asp) for their python tutorials. There are some differences between Python and CircuitPython, but most of the basic tutorials will carry over.
